753ca5c1dda85f85053d7166e565be577a53e5ca
dnomadb <damon@mapbox.com>
2022-02-10 07:29:29 -0800

    interpolation

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
-const sm = require('@mapbox/sphericalmercator');
+const tilebelt = require('@mapbox/tilebelt')
 const getPixels = require("get-pixels");
 const locking = require('@mapbox/locking');
+const interp = require("ndarray-linear-interpolate").d2;
+const ndarray = require("ndarray");
 
-const _getPixels = (url, callback) => {
+const _getPixels = locking((url, callback) => {
   return getPixels(url, null, callback);
-};
+});
 
-const cachedGetPixels = locking(_getPixels);
+
+const cachedGetPixels = (url) => {
+  return new Promise((resolve, reject) => {
+    _getPixels(url, (err, data) => {
+      if (err) reject(err);
+      return resolve(data);
+    })
+  });
+}
 
 
 class TerrainRGBquery {
@@ -17,7 +27,6 @@ class TerrainRGBquery {
     }
     this.endpoint = endpoint;
     this.tileSize = tileSize || 256;
-    this.merc = new sm({size: this.tileSize});
   };
 
   queryElevations(lnglats, zoom) {
@@ -26,33 +35,47 @@ class TerrainRGBquery {
     }));
   }
 
-  queryElevation(lnglat, zoom) {
+  interpolateElevation(data, x, y) {
+
+    const fx = Math.floor(x);
+    const fy = Math.floor(y);
+  
+    const grid = ndarray(new Float32Array(4), [2, 2]);
+    for (let i = fy; i < fy + 2; i++) {
+      for (let j = fx; j < fx + 2; j++) {
+        const r = data.get(j + 1, i + 1, 0)
+        const g = data.get(j + 1, i + 1, 1)
+        const b = data.get(j + 1, i + 1, 2);
+
+        grid.set(j - fx, i - fy, -10000 + ((r << 16) + (g << 8) + b) * 0.1);
+      }
+    }
+
+    let elev = interp(grid, x % fx, y % fy);
+
+    if (isNaN(elev)) {
+      const r = data.get(fx, fy, 0);
+      const g = data.get(fx, fy, 1);
+      const b = data.get(fx, fy, 2);
+      elev = -10000 + ((r << 16) + (g << 8) + b) * 0.1;
+    }
+
+    return elev;
+  }
+
+  async queryElevation(lnglat, zoom) {
     zoom = zoom || 14;
-    return new Promise((resolve, reject) => {
-      const px = this.merc.px(lnglat, zoom);
-      const xy = px.map((coord)=> {
-        return Math.floor(coord / this.tileSize);
-      });
-      const tile = `${zoom}/${xy[0]}/${xy[1]}`;
-      const url = this.endpoint.replace(/\{z\}\/\{x\}\/\{y\}/, tile);
 
-      return cachedGetPixels(url, (err, resp) => {
-        if (err) return reject(err);
+    const [x, y] = tilebelt.pointToTileFraction(lnglat[0], lnglat[1], zoom)
+    const pX = (x % Math.floor(x)) * this.tileSize;
+    const pY = (y % Math.floor(y)) * this.tileSize;
 
-        const tilePx = this.merc.px(lnglat, zoom).map((coord) => {
-          return Math.floor(coord % this.tileSize);
-        });
+    const tile = `${zoom}/${Math.floor(x)}/${Math.floor(y)}`;
+    const url = this.endpoint.replace(/\{z\}\/\{x\}\/\{y\}/, tile);
 
-        const stride = 4; //argb channels, one per channel
-        // desired pixel position is at row number * pixels per row, plus column number, and the sum multiplied by stride length
-        const pxIndex = stride * (tilePx[0] + tilePx[1] * this.tileSize);
+    const data = await cachedGetPixels(url);
 
-        const [r,g,b] = [resp.data[pxIndex], resp.data[pxIndex + 1], resp.data[pxIndex + 2]];
-        const elevation = -10000 + ((r * 256 * 256 + g * 256 + b) * 0.1);
-
-        return resolve(elevation);
-      });
-    });
+    return this.interpolateElevation(data, pX, pY);
   };
 };
 
